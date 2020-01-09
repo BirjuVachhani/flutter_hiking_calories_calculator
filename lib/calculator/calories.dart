1947dc5d8d136fce3d109af08340dbc8d34bed01
BirjuVachhani <brvachhani@gmail.com>
2020-01-09 13:51:34 +0530

    :memo: update copyrights
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

--- lib/calculator/calories.dart
+++ lib/calculator/calories.dart
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2020 Simform Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Author: Birju Vachhani (https://github.com/birjuvachhani)
 // Created Date: January 03, 2020
 
@@ -11,15 +27,14 @@ class Calories implements Comparable<Calories> {
 
   static const Calories zero = Calories._watts(0);
 
-  const Calories(
-      {double watts = 0,
-      double calPerHour = 0,
-      double calPerSecond = 0,
-      double calPerMinute = 0})
+  const Calories({double watts = 0,
+    double calPerHour = 0,
+    double calPerSecond = 0,
+    double calPerMinute = 0})
       : this._watts(watts +
-            (_equationFactor * calPerHour) +
-            (_equationFactor * calPerSecond * 3600) +
-            (_equationFactor * calPerMinute * 60));
+      (_equationFactor * calPerHour) +
+      (_equationFactor * calPerSecond * 3600) +
+      (_equationFactor * calPerMinute * 60));
 
   const Calories._watts(this._calories);
 
