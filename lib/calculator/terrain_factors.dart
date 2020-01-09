a37d1f6e8798dd795f1a7a44a6c32f6eb6e7fd78
BirjuVachhani <brvachhani@gmail.com>
2020-01-09 13:48:52 +0530

    :memo: update copyrights
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

--- lib/calculator/terrain_factors.dart
+++ lib/calculator/terrain_factors.dart
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2020 Simform Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Author: Birju Vachhani (https://github.com/birjuvachhani)
 // Created Date: January 03, 2020
 
@@ -41,11 +57,11 @@ class Terrain implements Comparable<Terrain> {
 
 class Terrains {
   static const Terrain PAVED_ROAD =
-      Terrain('Paved Road', 1.0, TerrainFactor.PAVED_ROAD);
+  Terrain('Paved Road', 1.0, TerrainFactor.PAVED_ROAD);
   static const Terrain GRAVEL_ROAD =
-      Terrain('Gravel Road', 1.2, TerrainFactor.GRAVEL_ROAD);
+  Terrain('Gravel Road', 1.2, TerrainFactor.GRAVEL_ROAD);
   static const Terrain WET_CLAY_OR_ICE =
-      Terrain('Wet Clay/Ice', 1.7, TerrainFactor.WET_CLAY_OR_ICE);
+  Terrain('Wet Clay/Ice', 1.7, TerrainFactor.WET_CLAY_OR_ICE);
   static const Terrain SAND = Terrain('Sand', 2.0, TerrainFactor.SAND);
   static const Terrain SWAMP = Terrain('Swamp', 3.5, TerrainFactor.SWAMP);
 }
